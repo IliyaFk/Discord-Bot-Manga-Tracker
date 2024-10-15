d1de62e7608a92229d510acacc4ab321cf60c451
IliyaFk <iliyafrouk1@gmail.com>
2024-10-15 16:25:13 -0400

    Added user and manga tracking

--- Manga-Updater.py
+++ Manga-Updater.py
@@ -15,6 +15,7 @@ clientDB = MongoClient(MONGO_URI)
 db = clientDB['DiscordDB']
 chapters_collection = db['chapters']
 chapters_collection.create_index([('number', 1)], unique=True)
+user_collection = db['users']
 
 ################################ Delete specific chapter from database for debugging purposes ############################
 # chapter_number_to_delete = 1133  # Change this to the chapter number you want to delete
@@ -34,8 +35,48 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Command to track a manga
 @bot.command(name='track')
 async def track(ctx, *, manga_name: str):
-    print(f'Command triggered by {ctx.author.name}')
-    await ctx.send(f'Tracking Manga: {manga_name}')
+    user_id = ctx.author.id
+    guild_id = ctx.guild.id
+
+    #Query the database for the user
+    user = user_collection.find_one({"user_id": str(user_id), "guild.guild.id": str(guild_id)})
+
+    if user:
+        for guild in user['guilds']:
+            if guild['guild_id'] == str(guild_id):
+                tracked_manga = guild.get('manga_tracking', [])
+                if not any(manga['manga_name'] == manga_name for manga in tracked_manga):
+                    guild['manga_tracking'].append({
+                        'manga_name': manga_name
+                    })
+                    user_collection.update_one(
+                        {"user_id": str(user_id), "guilds.guild_id": str(guild_id)},
+                        {"$set": {"guilds.$.manga_tracking": guild['manga_tracking']}}
+                    )
+                    await ctx.send(f"Started tracking {manga_name} for user {ctx.author.display_name}.")
+                else:
+                    await ctx.send(f"You are already tracking {manga_name}.")
+                break
+    else:
+        user_collection.insert_one({
+            "user_id": str(user_id),
+            "guilds": [
+                {
+                    "guild_id": str(guild_id),
+                    "manga_tracking": [
+                        {
+                            "manga_name": manga_name,
+                        }
+                    ]
+                }
+            ]
+        })
+        await ctx.send(f"Started tracking {manga_name} for user {ctx.author.display_name}.")
+
+    # print(f'Command triggered by {ctx.author.name}')
+    # await ctx.send(f'Tracking Manga: {manga_name}')
+    # print(f"Message received in Server ID: {ctx.guild.id}")
+    # print(f"Message received in channel ID: {ctx.channel.id}")
 
 # Simple ping command
 @bot.command(name='ping')
@@ -62,7 +103,29 @@ async def check_chapter_updates():
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    
+    # for guild in bot.guilds:
+    #     print(f"Connected to server: {guild.name} (ID: {guild.id})")
     bot.loop.create_task(check_chapter_updates())  # Start the background task
+
+
+#Function to get server and channel id of events
+# #Will be used to keep track of what server is keeeping track of what mangas
+# @bot.event
+# async def on_message(message):
+#     if message.guild: #Check if the message was sent in a server and not a dm
+#         guild_id = message.guild.id
+#         #print(f"Message received in Server ID: {guild_id}")
+
+#     if message.author == bot.user:
+#         return #Ignore messages sent by the bot
+    
+#     channel = message.channel
+#     channel_id = channel.id
+#     #print(f"Message received in channel ID: {channel_id}")
+
+#     await bot.process_commands(message)
+    
 
 # Run the bot
 bot.run(TOKEN)
